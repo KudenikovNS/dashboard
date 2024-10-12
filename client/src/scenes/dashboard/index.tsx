a4fdc11d2d35b9acc87f0fdefc78c2ab5e5d32b4
Nikita Kudenikov <KudenikovNS@yandex.ru>
2024-10-12 08:29:43 +0200

    Finish project

--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,18 +1,59 @@
 import { Box, useMediaQuery } from "@mui/material";
-import {
-  gridTemplateLargeScreens,
-  gridTemplateSmallScreens,
-} from "@/helpers/gridTemplate";
 import Row1 from "./Row1";
 import Row2 from "./Row2";
 import Row3 from "./Row3";
+
+const gridTemplateLargeScreens = `
+  "a b c"
+  "a b c"
+  "a b c"
+  "a b f"
+  "d e f"
+  "d e f"
+  "d h i"
+  "g h i"
+  "g h j"
+  "g h j"
+`;
+const gridTemplateSmallScreens = `
+  "a"
+  "a"
+  "a"
+  "a"
+  "b"
+  "b"
+  "b"
+  "b"
+  "c"
+  "c"
+  "c"
+  "d"
+  "d"
+  "d"
+  "e"
+  "e"
+  "f"
+  "f"
+  "f"
+  "g"
+  "g"
+  "g"
+  "h"
+  "h"
+  "h"
+  "h"
+  "i"
+  "i"
+  "j"
+  "j"
+`;
 
 const Dashboard = () => {
   const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)");
   return (
     <Box
       width="100%"
-      height="100%"
+      height="1px"
       display="grid"
       gap="1.5rem"
       sx={
@@ -23,8 +64,8 @@ const Dashboard = () => {
               gridTemplateAreas: gridTemplateLargeScreens,
             }
           : {
-              gridTemplateColumns: "1fr",
-              gridTemplateRows: "80px",
+              gridAutoColumns: "1fr",
+              gridAutoRows: "80px",
               gridTemplateAreas: gridTemplateSmallScreens,
             }
       }
--- server/index.js
+++ server/index.js
@@ -6,8 +6,12 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
+import transactionRoutes from "./routes/transaction.js";
 import KPI from "./models/KPI.js";
-import { kpis } from "./data/data.js"
+import Product from "./models/product.js"
+import Transaction from "./models/Transaction.js"
+import { kpis, products, transactions } from "./data/data.js"
 
 /* Configurations */
 dotenv.config();
@@ -22,6 +26,8 @@ app.use(cors());
 
 /* Routes */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
+app.use("/transaction", transactionRoutes);
 
 /* Mongoose setup */
 const PORT = process.env.PORT || 9000;
@@ -35,6 +41,8 @@ mongoose
     /* ADD DATA ONE TIME ONLY OR AS NEEDED */
       /* await mongoose.connection.db.dropDatabase(); //Удаляет предыдущую БД. В продакшен не надо это писать!!!
     KPI.insertMany(kpis); */
+    /* Product.insertMany(products); */
+    /* Transaction.insertMany(transactions); */
   })
   .catch((error) => console.log(`${error} did not connect`));
 
