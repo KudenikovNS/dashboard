78425fec17f9f3990b0015eeb870c7af6a2804fc
Nikita Kudenikov <KudenikovNS@yandex.ru>
2024-10-12 16:37:52 +0200

    Fix: padding App

--- client/src/scenes/navbar/index.tsx
+++ client/src/scenes/navbar/index.tsx
@@ -1,12 +1,9 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { Box, Typography, useTheme } from "@mui/material";
+import { Typography, useTheme } from "@mui/material";
 import PixIcon from "@mui/icons-material/Pix";
 import FlexBetween from "@/components/FlexBetween";
 
 const Navbar = () => {
   const { palette } = useTheme();
-  const [selected, setSelected] = useState("dashboard");
   return (
     <FlexBetween mb="0.25rem" p="0.5rem 0rem" color={palette.grey[300]}>
       {/* Левая сторона */}
@@ -15,33 +12,6 @@ const Navbar = () => {
         <Typography variant="h4" fontSize="16px">
           Finanseer
         </Typography>
-      </FlexBetween>
-      {/* Правая сторона */}
-      <FlexBetween gap="2rem">
-        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
-          <Link
-            to="/"
-            onClick={() => setSelected("dashboard")}
-            style={{
-              color: selected === "dashboard" ? "inherit" : palette.grey[700],
-              textDecoration: "inherit",
-            }}
-          >
-            dashboard
-          </Link>
-        </Box>
-        <Box sx={{ "&:hover": { color: palette.primary[100] } }}>
-          <Link
-            to="/predictions"
-            onClick={() => setSelected("predictions")}
-            style={{
-              color: selected === "predictions" ? "inherit" : palette.grey[700],
-              textDecoration: "inherit",
-            }}
-          >
-            predictions
-          </Link>
-        </Box>
       </FlexBetween>
     </FlexBetween>
   );
